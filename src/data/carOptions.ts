4716d1a0e4ff0d948299092f124e0fe08f0c1a20
Давид <david@Mac-mini-David.local>
2026-06-27 12:28:10 +0200

    Add structured listing form

---
+++ src/data/carOptions.ts
@@ -0,0 +1,96 @@
+export const carBrands = [
+  "Audi",
+  "BMW",
+  "Mercedes-Benz",
+  "Volkswagen",
+  "Opel",
+  "Ford",
+  "Skoda",
+  "Toyota",
+  "Honda",
+  "Mazda",
+  "Nissan",
+  "Renault",
+  "Peugeot",
+  "Citroën",
+  "Fiat",
+  "Hyundai",
+  "Kia",
+  "Seat",
+  "Volvo",
+  "Tesla",
+  "Porsche",
+  "Mini",
+  "Dacia",
+  "Mitsubishi",
+  "Suzuki",
+  "Subaru",
+  "Lexus",
+  "Land Rover",
+  "Jeep",
+  "Alfa Romeo",
+  "Chevrolet",
+  "Chrysler",
+  "Dodge",
+  "Jaguar",
+  "Smart",
+];
+
+export const bodyTypes = [
+  "Седан",
+  "Универсал",
+  "Хэтчбек",
+  "Купе",
+  "Кабриолет",
+  "Внедорожник / SUV",
+  "Кроссовер",
+  "Минивэн",
+  "Фургон",
+  "Пикап",
+  "Лимузин",
+];
+
+export const fuelTypes = [
+  "Бензин",
+  "Дизель",
+  "Газ / LPG",
+  "Гибрид",
+  "Plug-in Hybrid",
+  "Электро",
+  "Водород",
+];
+
+export const transmissions = ["Механика", "Автомат", "Робот", "Вариатор"];
+
+export const drivetrains = ["Передний", "Задний", "Полный"];
+
+export const doorCounts = ["2/3", "4/5"];
+
+export const seatCounts = ["2", "4", "5", "6", "7", "8+"];
+
+export const colors = [
+  "Белый",
+  "Чёрный",
+  "Серый",
+  "Серебристый",
+  "Синий",
+  "Красный",
+  "Зелёный",
+  "Коричневый",
+  "Бежевый",
+  "Жёлтый",
+  "Оранжевый",
+  "Фиолетовый",
+  "Золотой",
+  "Другой",
+];
+
+export const carConditions = [
+  "Новый",
+  "Б/у",
+  "После ДТП",
+  "Требует ремонта",
+  "Не на ходу",
+];
+
+export const sellerTypes = ["Частное лицо", "Автосалон", "Дилер"];
